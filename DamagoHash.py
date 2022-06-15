82c995fbd5aa6e2019ed08acad0f8d2575881ced
Darío <mrdarkiox@gmail.com>
2022-06-15 18:10:49 -0400

    Agregar comentarios, agregar opc 3

--- DamagoHash.py
+++ DamagoHash.py
@@ -1,42 +1,58 @@
-
-import base64,time
-
+import base64,time,os
+#Función de hash principal
 def DamagoHash(porhashear):
-
     paso1 = []
-    aux = 3 #Valor arbitrario
     sum = 1
+    #Convertir caracteres a decimal
     for i in porhashear.encode():
+        #Si se llega al final de lo que se hashea, finalizar for
         if i == len(porhashear):
             break
+        #Si el decimal es par, sumar
         if i%2 == 0:
           sum += i
+        #Cualquier otro caso, multiplicar
         else:
           sum *= i
-    #print("sum = ",sum)
+    #Convertir la suma a una lista de un string. (Se obtiene caracter por caracter)
     paso1 = list(str(sum))
     paso2 = []
+    #Convertir a binario y eliminar 0b inicial, luego guardar en paso2
     for i in paso1:
       a,b = bin(int(i)).split('b')
       paso2.append(b)
     paso3 = ""
+    #Pasar los binarios de paso2 a un string paso3
     for i in paso2:
       paso3 += i
     paso4 = ""
+    #Realizar un XOR en bloques de a 2 (caracter 0 con caracter 1, caracter 2 con caracter 3, etc.)
+    #Agregar resultado de XOR a paso4
     for i in range(0,len(paso3)-1,2):
       paso4 += str(int(paso3[i])^int(paso3[i+1]))
+    #Agregar el 0b eliminado anteriormente para tener una cadena de bytes
     paso4 = "0b"+paso4
-    paso4 = int(paso4,2)
+    #Intentar convertir a int los bytes
+    try:
+      paso4 = int(paso4,2)
+    except:
+      paso4 = paso4
+    #Convertir lo anterior a base64
     paso4 = base64.b64encode(str(paso4).encode()).decode('ascii')
     cont = 1
+    #Si es más corto que 100 caracteres
+    #Alterna entre repetir el hash derecho y luego al revés
     while len(paso4) < 100:
       if cont % 2 == 0:
         paso4 += paso4
       else:
         paso4 += paso4[::-1]
       cont+=1
+    #Si es mayor a 100, volver a Hashear
+    #Recursividad
     if len(paso4) > 100:
       return DamagoHash(paso4)
+    #Retornar los 100 caracteres del hash creado
     return paso4[:100]
   
 def MainMenu():
@@ -64,9 +80,11 @@ def opcion():
         else:
             print("OPCIÓN NO VALIDA")
             return opcion()
+    #Evitar ingreso de caracteres
     except ValueError:
         print("ERROR... No se pueden ingresar letras")
         return opcion()
+    #Evitar CTRL+C
     except:
         print("ERROR... Respete las normas")
         return opcion()
@@ -77,25 +95,47 @@ while(True):
   if opc == 5:
     break
   if opc == 1:
+    #Opción 1, un string ingresado por teclado
     porhashear = input("Ingrese un string a hashear: ")
     print(DamagoHash(porhashear))
   if opc == 2:
-    #Opcion 2, un archivo completo
+    #Opción 2, un archivo completo
     print("")
     print("NOTA: El archivo debe estar localizado en el mismo lugar del script.")
     Archivo = input("Nombre del archivo con extensión: ")
+    print(" "+"-"*38+" ")
+
+    #Implementación de Buffer de tamaño 1024 bytes
+    #Este será equivalente a 1024 siempre cuando el archivo sea de un tamaño mayor
+    #Si es menor, el buffer será equivalente a la mitad del tamaño del archivo
+    size = os.stat(Archivo).st_size
+    if  size < 1024:
+      #Dividir tamaño del archivo en la mitad, y en caso de ser decimal
+      #Redondear hacia arriba
+      buffer = int(size/2) + (size % 2 > 0)
+    else:
+      buffer=1024
     #Timer
     timer = time.time()
-    #Implementación de Buffer de tamaño 1024 bytes
-    buffer=1024
     Filehash = ""
+    #Leer archivo con buffer
     with open(Archivo,'rb') as archivo:
-        while archivo.read(buffer):
-            bleidos = str(base64.b64encode(archivo.read(buffer)))
-            Filehash += bleidos
-            Filehash = DamagoHash(Filehash)
+      while archivo.read(buffer):
+          bleidos = str(base64.b64encode(archivo.read(buffer)))
+          Filehash += bleidos
+          Filehash = DamagoHash(Filehash)
     print(Filehash)
-    print(time.time() - timer)
-
-  #print(DamagoHash("corneta"))
-  #print(DamagoHash("cornetb"))
+    #Imprimir tiempo de ejecución
+    print("Execution time: " , time.time() - timer , " s")
+  if opc == 3:
+      #Opción 3, archivo línea a línea
+      print("")
+      print("NOTA: El archivo debe estar localizado en el mismo lugar del script.")
+      Archivo = input("Nombre del archivo con extensión: ")
+      print(" "+"-"*38+" ")
+      contalinea = 1
+      with open(Archivo) as arc:
+        for line in arc:
+          print(DamagoHash(line))
+          contalinea+=1
+      print("Lineas hasheadas: ", contalinea)
